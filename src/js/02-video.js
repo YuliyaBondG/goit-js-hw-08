dc25a68932b29c75fcc2b8bb56bf1191a95c5b35
YuliyaBondG <manyunka@ukr.net>
2023-02-24 10:46:51 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(currentTime, 1000));
+function currentTime(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error.name);
+    console.log(error.message);
+
+});
