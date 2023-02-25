06ee39c3b3bde99415ceea86274528e56137780a
YuliyaBondG <manyunka@ukr.net>
2023-02-25 19:43:02 +0200

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+
 const galleryContainer = document.querySelector('.gallery');
 
 const galleryMarkup = createImageGalleryMarkup(galleryItems);
